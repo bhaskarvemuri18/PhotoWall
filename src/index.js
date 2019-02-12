e463308cf9a71c19b476bf11f04ddf0fb98f277f
Bhaskar Vemuri <bhaskarvemuri18@gmail.com>
2019-02-12 16:54:17 -0500

    Basic Components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const tasks = ['Take out the trash', 'Shovel the drive', 'Walk a dog']
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class List extends Component {
+    render() {
+        return (
+            <ol>
+                {tasks.map((task, index) => <li key={index}>{task}</li>)}
+            </ol>)
+    }
+}
+
+class Title extends Component {
+    render() {
+        return <h1>Task List</h1>
+    }
+}
+
+
+class Main extends Component {
+    render() {
+        return <div>
+            <Title />
+            <List />
+        </div>
+    }
+}
+
+
+
+ReactDOM.render(<Main />, document.getElementById('root'));

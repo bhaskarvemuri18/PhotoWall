89669c7992b35e355a31d83e81e2ad70ed3726c9
Bhaskar Vemuri <bhaskarvemuri18@gmail.com>
2019-02-12 18:52:36 -0500

    Stateless functions and remove

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,31 +2,47 @@ import React, { Component } from 'react';
 import Title from './Title'
 import PhotoWall from './PhotoWall'
 
-const posts = [
-    {
-        id: "0",
-        description: "Red Apples!",
-        imageLink: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX1065583.jpg"
-    },
-    {
-        id: "1",
-        description: "Mangoes yummy!!",
-        imageLink: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX5470556.jpg"
-    },
-    {
-        id: "2",
-        description: "Juicy Watermelons!!",
-        imageLink: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX14919593.jpg"
-    }
-]
-
 
 
 class Main extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            posts: [
+                {
+                    id: "0",
+                    description: "Red Apples!",
+                    imageLink: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX1065583.jpg"
+                },
+                {
+                    id: "1",
+                    description: "Mangoes yummy!!",
+                    imageLink: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX5470556.jpg"
+                },
+                {
+                    id: "2",
+                    description: "Juicy Watermelons!!",
+                    imageLink: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX14919593.jpg"
+                }
+            ]
+        }
+        this.removePhoto = this.removePhoto.bind(this);
+    }
+
+
+    removePhoto(postRemoved) {
+        console.log(postRemoved.description);
+        this.setState((state) => ({
+            posts: state.posts.filter(post => post !== postRemoved)
+        }))
+    }
+
+
     render() {
         return <div>
             <Title title={"PhotoWall"} />
-            <PhotoWall posts={posts} />
+            <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto} />
         </div>
     }
 }
--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
+// Stateless Functions
 
-class Title extends Component {
-    render() {
-        return <h1>{this.props.title}</h1>
-    }
+function Title(props) {
+    return <h1>{props.title}</h1>
 }
+
 
 export default Title

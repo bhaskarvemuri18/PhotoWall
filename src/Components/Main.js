4a5292420517adf1d25e42112ea60cdf499f474d
Bhaskar Vemuri <bhaskarvemuri18@gmail.com>
2019-02-12 18:21:55 -0500

    PhotoWall layout

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import List from './List'
 import Title from './Title'
+import PhotoWall from './PhotoWall'
+
+const posts = [
+    {
+        id: "0",
+        description: "Red Apples!",
+        imageLink: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX1065583.jpg"
+    },
+    {
+        id: "1",
+        description: "Mangoes yummy!!",
+        imageLink: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX5470556.jpg"
+    },
+    {
+        id: "2",
+        description: "Juicy Watermelons!!",
+        imageLink: "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX14919593.jpg"
+    }
+]
+
+
 
 class Main extends Component {
     render() {
         return <div>
-            <Title title={"To-dos"} />
-            <List tasks={['Move the lawn', 'Walk the dog']} />
-            <List tasks={['Hose driveway', 'Finish laundry']} />
+            <Title title={"PhotoWall"} />
+            <PhotoWall posts={posts} />
         </div>
     }
 }
